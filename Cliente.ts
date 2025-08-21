6faa7f90b990764e5680293b59bf3cba12334351
Gustavo <artoriasboy@gmail.com>
2025-08-21 11:33:36 -0300

    Adicionar construtor

--- Cliente.ts
+++ Cliente.ts
@@ -1,5 +1,12 @@
 export class Cliente{
-    nome: string = "";
-    telefone: string = "";
-    cpf: string = "";
+    nome: string;
+    telefone: string;
+    cpf: string;
+
+
+constructor(nome:string = "", telefone: string = "", cpf: string = "") {
+    this.nome = nome;
+    this.telefone = telefone;
+    this.cpf = cpf;
+}
 }
--- Main.ts
+++ Main.ts
@@ -1,3 +1,6 @@
 import { Cliente } from "./Cliente";
 
-const cli: Cliente = new Cliente()
+const cli: Cliente = new Cliente("Gustavo");
+cli.telefone = "55912312312";
+cli.cpf = "12312312312";
+console.table(cli);
